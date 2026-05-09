65d0218c4a5ac776bcdefdac02af5187fa3b017e
Kumar saurabh yadav <saurabhrahul101@gmail.com>
2026-05-09 13:12:11 +0530

    APIs created for employees, authentication, Attendance

--- ems_server/server.js
+++ ems_server/server.js
@@ -3,6 +3,10 @@ import cors from "cors";
 import "dotenv/config";
 import multer from "multer";
 import connectDB from "./config/db.js";
+import authRouter from "./routes/authRoutes.js";
+import employeeRouter from "./routes/employeeRoutes.js";
+import profileRouter from "./routes/profileRoutes.js";
+import attendaceRouter from "./routes/attendanceRoutes.js";
 
 
 const app = express()
@@ -16,6 +20,11 @@ app.use(multer().none())
 
 //Routes
 app.get("/", (req, res)=> res.send("server is running"))
+app.use("/api/auth", authRouter)
+app.use("/api/employees", employeeRouter)
+app.use("/api/profile", profileRouter)
+app.use("/api/attendance", attendaceRouter)
+
 
 await connectDB()
 
