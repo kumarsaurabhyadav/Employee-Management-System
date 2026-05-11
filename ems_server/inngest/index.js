968ca732ae2645f87ef5f4c72a66cb3572a2295a
Kumar saurabh yadav <saurabhrahul101@gmail.com>
2026-05-11 18:40:28 +0530

    fix email sent cron for attendance

--- ems_server/inngest/index.js
+++ ems_server/inngest/index.js
@@ -212,9 +212,11 @@ const attendanceReminderCron = inngest.createFunction(
                         `,
           });
         });
+        await Promise.all(emailPromises);
+        return {emailsSent: absentEmployees.length}
       });
     }
-    await Promise.all(emailPromises);
+  
     return {
       totalActive: activeEmployees.length,
       onLeave: onLeaveIds.length,
